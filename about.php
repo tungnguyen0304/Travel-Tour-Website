efb4158370c5d138e028dae27c9ab8f8e7b12c34
Nguyen Lam Tung <tung.nguyen.mauxox@gmail.com>
2023-07-04 19:21:36 +0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatiable" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- header section stars -->
+
+    <section class="header">
+
+        <a href="home.php" class="logo"> travel. </a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+    <!-- header section ends -->
+
+    <div class="heading" style="background:url(images/header-bg-1.jpg) no-repeat">
+        <h1>about us</h1>
+    </div>
+
+    <!-- about section stars -->
+
+    <section class="about">
+
+        <div class="image">
+            <img src="images/about-img.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Deserunt omnis alias ex debitis temporibus iusto aliquid
+                soluta odio earum saepe numquam voluptates corrupti dolor
+                illum ab repudiandae, recusandae assumenda sapiente.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                At modi commodi itaque nihil architecto optio nam voluptatum
+                consequatur, quo perferendis.</p>
+
+            <div class="icons-container">
+
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>top destination</span>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>affordable price</span>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24/7 guide service</span>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
+    <!-- about section ends -->
+
+    <!-- reviews section stars -->
+
+    <section class="reviews">
+
+        <div class="swiper reviews-sliders">
+
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat autem nihil earum explicabo soluta quidem praesentium minus commodi, asperiores distinctio nobis quis quo eligendi ducimus, recusandae delectus. Placeat, fugit numquam!</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-1.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus cupiditate quas dicta. Culpa odio quae atque omnis explicabo corporis in id nulla, quidem delectus exercitationem accusantium et repellat rem eum deserunt ut quas tempora voluptatem nam rerum. Rem, aliquid nihil.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-2.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat autem nihil earum explicabo soluta quidem praesentium minus commodi, asperiores distinctio nobis quis quo eligendi ducimus, recusandae delectus. Placeat, fugit numquam!</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-3.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam reprehenderit voluptates eligendi cum numquam sint?</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-4.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos reiciendis quasi eos dolorum voluptatum aliquam fuga quisquam rerum, soluta libero dolorem delectus eligendi. Dolor nam iusto fugit perspiciatis velit corrupti vel beatae quos alias.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-5.png" alt="">
+                </div>
+
+                <div class="swiper-slide slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus culpa et magni impedit labore enim, iusto ducimus eveniet accusantium repellat./p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="images/pic-6.png" alt="">
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- reviews section ends -->
+
+    <!-- footer section stars -->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+                <a href="#"> <i class="fas fa-phone"></i> +111-222-3333</a>
+                <a href="#"> <i class="fas fa-envelope"></i> daothang@gmail.com</a>
+                <a href="#"> <i class="fas fa-map"></i> Q12, TP HCM</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit">created by <span>Nguyen Lam Tung</span> | all rights reserved! </div>
+
+    </section>
+
+    <!-- footer section ends -->
+
+    <!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link -->
+    <script src="script.js"></script>
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,73 @@
+// Nó lấy hai phần tử HTML từ trang: một phần tử có id là menu-btn
+let menu = document.querySelector("#menu-btn");
+// một phần tử có class là navbar nằm trong một phần tử có class là header
+let navbar = document.querySelector(".header .navbar");
+
+// định nghĩa một hàm xử lý sự kiện cho sự kiện onclick của phần tử menu
+menu.onclick = () => {
+  // Khi người dùng nhấp vào phần tử menu, hàm xử lý sự kiện sẽ được gọi và nó sẽ thêm hoặc xóa class fa-times cho phần tử menu
+  menu.classList.toggle("fa-times");
+  // và class active cho phần tử navbar
+  navbar.classList.toggle("active");
+};
+
+// Nó định nghĩa một hàm sẽ được thực thi khi người dùng cuộn trang.
+window.onscroll = () => {
+  // nó sẽ gỡ bỏ class fa-times khỏi phần tử menu
+  menu.classList.remove("fa-times");
+  //   gỡ bỏ class active khỏi phần tử navbar
+  navbar.classList.remove("active");
+};
+
+// sử dụng thư viện Swiper
+// Nó tạo ra một đối tượng Swiper mới cho phần tử có class home-slider
+var swiper = new Swiper(".home-slider", {
+  // khi cuộn đến cuối danh sách, nó sẽ quay lại đầu danh sách
+  loop: true,
+  // Thuộc tính navigation cho phép người dùng điều hướng qua lại giữa các slide
+  // bằng cách sử dụng các nút có class swiper-button-next và swiper-button-prev
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".reviews-sliders", {
+  loop: true,
+  // khoảng cách 20px giữa các slide
+  spaceBetween: 20,
+  // chiều cao của Swiper sẽ tự động thay đổi để phù hợp với chiều cao của slide hiện tại
+  autoHeight: true,
+  // con trỏ chuột sẽ thay đổi thành hình bàn tay khi di chuột qua Swiper để người dùng biết rằng họ có thể kéo Swiper
+  grabCursor: true,
+  // số lượng slide hiển thị trên một dòng sẽ thay đổi tùy thuộc vào kích thước màn hình
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// chọn phần tử có class là btn nằm trong phần tử có class là load-more, nằm trong phần tử có class là packages
+let loadMoreBtn = document.querySelector(".packages .load-more .btn");
+let currentItem = 3;
+
+// gán một hàm ẩn danh cho sự kiện onclick của phần tử được lưu trữ trong biến loadMoreBtn. Điều này có nghĩa là khi người dùng nhấp vào phần tử này, hàm ẩn danh sẽ được thực thi.
+loadMoreBtn.onclick = () => {
+  //  Dòng này khai báo một biến boxes và gán giá trị cho nó bằng một mảng các phần tử được trả về từ phương thức querySelectorAll với đối số là chuỗi CSS selector .packages .box-container .box
+  // Điều này có nghĩa là chọn tất cả các phần tử có class là box nằm trong phần tử có class là box-container, nằm trong phần tử có class là packages.
+  let boxes = [...document.querySelectorAll(".packages .box-container .box")];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 3;
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+};
